e9195c77f928cdcc1d0456e0fe35120cb8a32366
ItzDC2 <donovancf12380@gmail.com>
2023-01-19 12:28:22 +0000

    Añadido efectos de sonido MainMenu

--- src/main/java/controllers/MainMenuController.java
+++ src/main/java/controllers/MainMenuController.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import dad.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -27,6 +29,9 @@ public class MainMenuController implements Initializable{
     @FXML 
     private Button salirButton;
     
+    private MediaPlayer backgroundPlayer = getMediaPlayer("/sounds/background-theme.mp3");
+    
+    
     public MainMenuController() {
         try{
             FXMLLoader l = new FXMLLoader(getClass().getResource("/fxml/MainMenuView.fxml"));
@@ -43,19 +48,24 @@ public class MainMenuController implements Initializable{
 
     public void initialize(URL location, ResourceBundle resources){
     	
-    	
-    	//game.init();
+    	backgroundPlayer.setOnEndOfMedia(() -> backgroundPlayer.play());
+    	backgroundPlayer.setVolume(0.1);
+    	backgroundPlayer.play();
     	
     }
-    
-    private MediaPlayer getSonido(String path) {
+    	
+    private MediaPlayer getMediaPlayer(String path) {
     	return new MediaPlayer(new Media(getClass().getResource(path).toString()));
+    }
+    
+    @FXML
+    private void onMouseEnter(MouseEvent event) {
+    	getMediaPlayer("/sounds/whoosh.mp3").play();
     }
     
     @FXML
     private void onJugarAction(ActionEvent event) {
     	//code
-    	getSonido("/sounds/wet-fart.mp3").play();
     }
 
     @FXML
@@ -65,7 +75,7 @@ public class MainMenuController implements Initializable{
 
     @FXML
     private void onSalirAction(ActionEvent event) {
-    	//code
+    	App.primaryStage.close();
     }
 
 }
--- src/main/java/dad/App.java
+++ src/main/java/dad/App.java
@@ -2,15 +2,18 @@ package dad;
 
 import controllers.MainMenuController;
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 public class App extends Application{
 
+	public static Stage primaryStage;
+	
     @Override public void start(Stage stage) throws Exception {
 
         MainMenuController mc = new MainMenuController();
+        App.primaryStage = stage;
         stage.setTitle("ANTARTIC RPG");
         stage.setScene(new Scene(mc.getView()));
         stage.show();
