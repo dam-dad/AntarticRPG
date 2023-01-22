2cfd6b2d086c8a9683d607704998dad6c041ae6c
ItzDCraft <donovancf12380@gmail.com>
2023-01-22 19:12:31 +0000

    Terminado MainMenu

--- src/main/java/controllers/MainMenuController.java
+++ src/main/java/controllers/MainMenuController.java
@@ -5,21 +5,36 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import dad.App;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class MainMenuController implements Initializable{
 
+	private DoubleProperty musicVolume = new SimpleDoubleProperty();
+	private DoubleProperty sfxVolume = new SimpleDoubleProperty();
+	
     @FXML 
     private BorderPane view;
 
+    @FXML
+    private ImageView titleView;
+    
+    @FXML
+    private StackPane pane;
+    
     @FXML 
     private Button jugarButton;
     
@@ -29,12 +44,14 @@ public class MainMenuController implements Initializable{
     @FXML 
     private Button salirButton;
     
-    private MediaPlayer backgroundPlayer = getMediaPlayer("/sounds/background-theme.mp3");
+    private MediaPlayer musicPlayer = getMediaPlayer("/sounds/background-theme.mp3");
+    private MediaPlayer sfxPlayer = new MediaPlayer(new Media(getClass().getResource("/sounds/whoosh.mp3").toString()));
     
+    private OptionsMenuController optionsMenuController = new OptionsMenuController();
     
     public MainMenuController() {
         try{
-            FXMLLoader l = new FXMLLoader(getClass().getResource("/fxml/MainMenuView.fxml"));
+            FXMLLoader l = new FXMLLoader(getClass().getResource("/fxml/mainMenu/MainMenuView.fxml"));
             l.setController(this);
             l.load();
         } catch (IOException e){
@@ -48,19 +65,38 @@ public class MainMenuController implements Initializable{
 
     public void initialize(URL location, ResourceBundle resources){
     	
-    	backgroundPlayer.setOnEndOfMedia(() -> backgroundPlayer.play());
-    	backgroundPlayer.setVolume(0.1);
-    	backgroundPlayer.play();
+    	//listeners
+    	musicVolume.addListener(this::refreshMasterVolume);
+    	sfxVolume.addListener(this::refreshSFXVolume);
+    	
+    	sfxVolume.bind(optionsMenuController.sfxVolumeProperty());
+    	musicVolume.bind(optionsMenuController.musicVolumeProperty());
+    	
+    	//actions
+    	musicPlayer.setOnEndOfMedia(() -> musicPlayer.play());
+    	musicPlayer.setVolume(musicVolume.multiply(100).divide(1).get());
+    	musicPlayer.play();
+    	
+    	optionsMenuController.setMainMenuController(this);
     	
     }
-    	
-    private MediaPlayer getMediaPlayer(String path) {
+    
+	private void refreshSFXVolume(ObservableValue<? extends Number> o, Number ov, Number nv) {
+		sfxPlayer.setVolume(sfxVolume.get() != 0 ? sfxVolume.divide(100).get() : 0);
+	}
+
+	private void refreshMasterVolume(ObservableValue<? extends Number> o, Number ov, Number nv) {
+		musicPlayer.setVolume(musicVolume.get() != 0 ? musicVolume.divide(100).get() : 0);
+	}
+
+	private MediaPlayer getMediaPlayer(String path) {
     	return new MediaPlayer(new Media(getClass().getResource(path).toString()));
     }
     
     @FXML
-    private void onMouseEnter(MouseEvent event) {
-    	getMediaPlayer("/sounds/whoosh.mp3").play();
+    private void onMouseEntered(MouseEvent event) {
+    	sfxPlayer.stop();
+    	sfxPlayer.play();
     }
     
     @FXML
@@ -70,12 +106,24 @@ public class MainMenuController implements Initializable{
 
     @FXML
     private void onOptionsAction(ActionEvent event) {
-    	//code	
+    	pane.getChildren().add(optionsMenuController.getView());
+    	
+    	StackPane.setMargin(optionsMenuController.getView(), new Insets(-150, 0,0,0));
+    	optionsMenuController.getView().setPrefSize(600, BorderPane.USE_COMPUTED_SIZE);
+    	optionsMenuController.getView().toFront();
     }
 
     @FXML
     private void onSalirAction(ActionEvent event) {
     	App.primaryStage.close();
     }
-
+    
+    public MediaPlayer getSfxPlayer() {
+		return sfxPlayer;
+	}
+    
+    public StackPane getPane() {
+		return pane;
+	}
+    
 }
---
+++ src/main/java/controllers/OptionsMenuController.java
@@ -0,0 +1,176 @@
+package controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXSlider;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+
+public class OptionsMenuController implements Initializable {
+
+	private DoubleProperty musicVolume = new SimpleDoubleProperty();
+	private DoubleProperty sfxVolume = new SimpleDoubleProperty();
+	
+	private BooleanProperty sfxMuted = new SimpleBooleanProperty();
+	private BooleanProperty musicMuted = new SimpleBooleanProperty();
+	
+	
+	private MainMenuController mainMenuController;
+
+	@FXML
+	private ImageView sfxIcon;
+	
+	@FXML
+	private ImageView musicIcon;
+	
+	@FXML
+	private JFXSlider musicSlider;
+
+	@FXML
+	private JFXSlider sfxSlider;
+
+	@FXML
+	private Label musicLabel;
+
+	@FXML
+	private Label sfxLabel;
+
+	@FXML
+	private BorderPane view;
+
+	public OptionsMenuController() {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/mainMenu/OptionsMenuView.fxml"));
+		loader.setController(this);
+		try {
+			loader.load();
+		} catch (IOException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		// bindings
+		sfxVolume.bind(sfxSlider.valueProperty());
+		musicVolume.bind(musicSlider.valueProperty());
+		
+		sfxLabel.textProperty().bind(sfxVolume.asString("%1.0f").concat("%"));
+		musicLabel.textProperty().bind(musicVolume.asString("%1.0f").concat("%"));
+
+	}
+
+	@FXML
+	private void onAceptarAction(ActionEvent e) {
+		mainMenuController.getPane().getChildren().remove(view);
+	}
+
+	@FXML
+	private void onMouseEntered(MouseEvent e) {
+		mainMenuController.getSfxPlayer().stop();
+		mainMenuController.getSfxPlayer().play();
+	}
+
+    @FXML
+    private void onMusicClick(MouseEvent event) {
+    	if(!isMusicMuted()) {
+    		musicVolume.unbind();
+    		musicIcon.setImage(new Image(getClass().getResourceAsStream("/assets/mute-music.png")));
+    		setMusicVolume(0);
+    		setMusicMuted(true);
+    	} else {
+    		musicVolume.bind(musicSlider.valueProperty());
+    		musicIcon.setImage(new Image(getClass().getResourceAsStream("/assets/music.png")));
+    		musicMuted.set(false);
+    	}
+    }
+
+    @FXML
+    private void onSFXClick(MouseEvent event) {
+    	if(!isSfxMuted()) {
+    		sfxVolume.unbind();
+    		sfxIcon.setImage(new Image(getClass().getResourceAsStream("/assets/mute-sfx.png")));
+    		setSfxVolume(0);
+    		setSfxMuted(true);
+    	} else {
+    		sfxVolume.bind(sfxSlider.valueProperty());
+    		sfxIcon.setImage(new Image(getClass().getResourceAsStream("/assets/sfx.png")));
+    		sfxMuted.set(false);
+    	}
+    }
+    
+	public BorderPane getView() {
+		return view;
+	}
+
+	public final DoubleProperty musicVolumeProperty() {
+		return this.musicVolume;
+	}
+
+	public final double getMusicVolume() {
+		return this.musicVolumeProperty().get();
+	}
+
+	public final void setMusicVolume(final double musicVolume) {
+		this.musicVolumeProperty().set(musicVolume);
+	}
+
+	public final DoubleProperty sfxVolumeProperty() {
+		return this.sfxVolume;
+	}
+
+	public final double getSfxVolume() {
+		return this.sfxVolumeProperty().get();
+	}
+
+	public final void setSfxVolume(final double sfxVolume) {
+		this.sfxVolumeProperty().set(sfxVolume);
+	}
+
+	public MainMenuController getMainMenuController() {
+		return mainMenuController;
+	}
+
+	public void setMainMenuController(MainMenuController mainMenuController) {
+		this.mainMenuController = mainMenuController;
+	}
+
+	public final BooleanProperty sfxMutedProperty() {
+		return this.sfxMuted;
+	}
+
+	public final boolean isSfxMuted() {
+		return this.sfxMutedProperty().get();
+	}
+
+	public final void setSfxMuted(final boolean sfxMuted) {
+		this.sfxMutedProperty().set(sfxMuted);
+	}
+
+	public final BooleanProperty musicMutedProperty() {
+		return this.musicMuted;
+	}
+
+	public final boolean isMusicMuted() {
+		return this.musicMutedProperty().get();
+	}
+
+	public final void setMusicMuted(final boolean musicMuted) {
+		this.musicMutedProperty().set(musicMuted);
+	}
+
+}
